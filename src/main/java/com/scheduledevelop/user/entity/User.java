ffbb637fad900ff6ae7c4ca2e9e7e74624ee47db
usersy628 <kimsy628@gmail.com>
2026-04-20 11:31:57 +0900

    feat: add unique constraint to User email field
    email: unique = true 제약 조건 추가

--- src/main/java/com/scheduledevelop/user/entity/User.java
+++ src/main/java/com/scheduledevelop/user/entity/User.java
@@ -19,7 +19,7 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private String userName;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     public User(String userName, String email) {
